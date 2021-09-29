df4f2d468f99ff7404e077545f656152f9219d9e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-29 23:13:33 +0900

    add some files

--- zscaler-admin-app/main.py
+++ zscaler-admin-app/main.py
@@ -1,23 +1,16 @@
 import typer
+from zscaler_python_sdk import url_categories
+
+# from zscaler_python_sdk import url_categories
 
 app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
-
-
-@app.command()
 def adminuser(cmd: str):
+    # if cmd is None:
+    #     typer.echo("Please set correct cmd after `adminuser`")
+
     sample_result = """
         {
             "id":45593290,
@@ -39,11 +32,44 @@ def adminuser(cmd: str):
         "name":"Yuta Kawamura"
     }
     """
+
     if cmd == "ls":
         typer.echo(sample_result)
-    else:
+
+
+@app.command()
+def urlcategory(cmd: str):
+    if cmd is None:
         typer.echo("Please set correct cmd after `adminuser`")
+
+    sample_result = """
+        {
+            "id":45593290,
+            "loginName":"yuta.kawamura@zscaler.net",
+            "userName":"Yuta Kawamura",
+            "email":"yuta.kawamura@zscaler.net",
+            "role":{"id":41577,"name":"Super Admin",
+            "isNameL10nTag":true,
+            "extensions":{
+                "adminRank":"0",
+                "roleType":"EXEC_INSIGHT_AND_ORG_ADMIN"
+            }
+        },
+        "adminScopescopeGroupMemberEntities":[],
+        "adminScopeType":"ORGANIZATION",
+        "adminScopeScopeEntities":[],
+        "isPasswordLoginAllowed":true,
+        "pwdLastModifiedTime":1622193781,
+        "name":"Yuta Kawamura"
+    }
+    """
+
+    if cmd == "ls":
+        typer.echo(sample_result)
+
 
 
 if __name__ == "__main__":
+    
+    print(url_categories.fetch_url_categories())
     app()
